b46016a683f79f624b10659f4a070340b775d5fa
mjcarnaje <michaeljamescarnaje1@gmail.com>
2020-08-18 16:08:03 +0800

    css new folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import "./assets/main.css";
+import "./assets/css/main.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
