a9ba6f81b1f2727ec06ccd57247370d224cf5bcd
mjcarnaje <michaeljamescarnaje1@gmail.com>
2020-09-10 13:04:11 +0800

    Updates

---
+++ precache-manifest.05c3b44a8f54df741e69fe5793798aa6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b9e19a60a465bd1452fa6331f53532",
+    "url": "/reviewer-maker-app/index.html"
+  },
+  {
+    "revision": "ef43ca850e3637bc1b95",
+    "url": "/reviewer-maker-app/static/css/main.4b6da885.chunk.css"
+  },
+  {
+    "revision": "55e6f3ac061cb564329e",
+    "url": "/reviewer-maker-app/static/js/2.3c505626.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reviewer-maker-app/static/js/2.3c505626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef43ca850e3637bc1b95",
+    "url": "/reviewer-maker-app/static/js/main.d411bd93.chunk.js"
+  },
+  {
+    "revision": "147ac37545bd0beafddd",
+    "url": "/reviewer-maker-app/static/js/runtime-main.29455b6d.js"
+  },
+  {
+    "revision": "1fb5c281a8a4beac71cd0e6fc3bc244a",
+    "url": "/reviewer-maker-app/static/media/add_question.1fb5c281.svg"
+  },
+  {
+    "revision": "8169a43657c7301537fad0e30c2f028a",
+    "url": "/reviewer-maker-app/static/media/create_question.8169a436.svg"
+  },
+  {
+    "revision": "7d8bfcef31e166a58f07bb7a21cfb52c",
+    "url": "/reviewer-maker-app/static/media/start_question.7d8bfcef.svg"
+  },
+  {
+    "revision": "6beea01b0868d84717d31b223f6d33a0",
+    "url": "/reviewer-maker-app/static/media/studying.6beea01b.svg"
+  }
+]);
