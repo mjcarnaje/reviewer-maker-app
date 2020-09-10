0e1e032d0be0c83b0485de37ba9dfed840d01eb4
mjcarnaje <michaeljamescarnaje1@gmail.com>
2020-09-10 13:07:20 +0800

    initial deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './assets/css/main.css';
 import App from './App';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
